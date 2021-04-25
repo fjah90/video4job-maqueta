aeda95570bbae130e77da2090226cdb5b1f18714
Brayyan <bayomc@gmail.com>
2021-04-25 16:32:23 -0400

    bottom-navigation-example

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -6,25 +6,26 @@ import { ColorSchemeName } from 'react-native';
 
 import { RootStackParamList } from '../types';
 import LinkingConfiguration from './LinkingConfiguration';
-import NotFoundScreen from '@screens/NotFoundScreen';
-import LoginScreen from '@screens/SignIn/LoginScreen';
-import RegisterScreen from '@screens/SignUp/StepOneScreen';
-import LocationScreen from '@screens/SignUp/StepTwoScreen';
-import TermsScreen from '@screens/SignUp/StepThreeScreen';
-import BusinessDataScreen from '@screens/SignUp/business/FormBusinessDataScreen';
-import OfertCreateScreen from '@screens/SignUp/business/FormOfertCreateScreen';
-import FlashCardsScreen from '@screens/FlashCards/FlashCardsScreen';
-import EmailLoginScreen from '@screens/SignIn/SignInEmailScreen';
-import FlashFilterScreen from '@screens/FlashCards/FlashFilterScreen';
-import FlashCandidateProfileScreen from '@screens/FlashCards/FlashCandidateProfileScreen';
-import ListCandidateScreen from '@screens/Lists/ListCandidateScreen';
-import ListFavoritesScreen from '@screens/Lists/ListFavoritesScreen';
-import ListOffersScreen from '@screens/Lists/ListOffersScreen';
-import ProfileBusinessScreen from '@screens/Profile/ProfileBusinessScreen';
-import MyShopListScreen from '@screens/Profile/MyShopListScreen';
-import ShoppingCardScreen from '@screens/Checkout/ShoppingCardScreen';
-import WayToPayScreen from '@screens/Checkout/WayToPayScreen';
-import SuccessScreen from '@screens/Checkout/SuccessScreen';
+//import NotFoundScreen from '@creens/NotFoundScreen';
+import LoginScreen from '../src/screens/SignIn/LoginScreen';
+import RegisterScreen from '../src/screens/SignUp/StepOneScreen';
+import LocationScreen from '../src/screens/SignUp/StepTwoScreen';
+import TermsScreen from '../src/screens/SignUp/StepThreeScreen';
+import BusinessDataScreen from '../src/screens/SignUp/business/FormBusinessDataScreen';
+import OfertCreateScreen from '../src/screens/SignUp/business/FormOfertCreateScreen';
+import FlashCardsScreen from '../src/screens/FlashCards/FlashCardsScreen';
+import EmailLoginScreen from '../src/screens/SignIn/SignInEmailScreen';
+import FlashFilterScreen from '../src/screens/FlashCards/FlashFilterScreen';
+import FlashCandidateProfileScreen from '../src/screens/FlashCards/FlashCandidateProfileScreen';
+import ListCandidateScreen from '../src/screens/Lists/ListCandidateScreen';
+import ListFavoritesScreen from '../src/screens/Lists/ListFavoritesScreen';
+import ListOffersScreen from '../src/screens/Lists/ListOffersScreen';
+import ProfileBusinessScreen from '../src/screens/Profile/ProfileBusinessScreen';
+import MyShopListScreen from '../src/screens/Profile/MyShopListScreen';
+import ShoppingCardScreen from '../src/screens/Checkout/ShoppingCardScreen';
+import WayToPayScreen from '../src/screens/Checkout/WayToPayScreen';
+import SuccessScreen from '../src/screens/Checkout/SuccessScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
@@ -42,6 +43,18 @@ export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeNa
 // Read more here: https://reactnavigation.org/docs/modal
 const Stack = createStackNavigator<RootStackParamList>();
 
+const BottomTabs = createBottomTabNavigator();
+
+const BottomNavigator = ()=>{
+  return(
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="Candidate" component={ListCandidateScreen} />
+      <BottomTabs.Screen name="Favorites" component={ListFavoritesScreen} />
+      <BottomTabs.Screen name="Offers" component={ListOffersScreen} />
+    </BottomTabs.Navigator>
+  )
+}
+
 function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
@@ -56,16 +69,17 @@ function RootNavigator() {
       <Stack.Screen name="FlashCards" component={FlashCardsScreen} options={{ title: 'SignIn Business > Flash Candidates' }} />
       <Stack.Screen name="FlashFilter" component={FlashFilterScreen} options={{ title: 'SignIn Business > Flash Filter' }} />
       <Stack.Screen name="FlashCandidateProfile" component={FlashCandidateProfileScreen} options={{ title: 'SignIn Business > Flash Filter' }} />
+      {/*TODO REMOVE*/}
       <Stack.Screen name="ListCandidate" component={ListCandidateScreen} options={{ title: 'List Candidates' }} />
       <Stack.Screen name="ListFavorites" component={ListFavoritesScreen} options={{ title: 'List Favorites' }} />
       <Stack.Screen name="ListOffers" component={ListOffersScreen} options={{ title: 'List Offers' }} />
+      {/*TODO REMOVE*/}
       <Stack.Screen name="ProfileBusiness" component={ProfileBusinessScreen} options={{ title: 'My Profile' }} />
       <Stack.Screen name="MyShopList" component={MyShopListScreen} options={{ title: 'My History Shop' }} />
       <Stack.Screen name="ShoppingCard" component={ShoppingCardScreen} options={{ title: 'My Shopping Card' }} />
       <Stack.Screen name="WayToPay" component={WayToPayScreen} options={{ title: 'Way To Pay' }} />
       <Stack.Screen name="Success" component={SuccessScreen} options={{ title: 'Success' }} />
-      {/* Candidates Stack */}
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+      <Stack.Screen name="List" component={BottomNavigator} />      
     </Stack.Navigator>
   );
 }
